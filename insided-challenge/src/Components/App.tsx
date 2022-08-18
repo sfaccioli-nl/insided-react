bcf1337a24efc510dd86bc5cbfe03c2e52896f6a
Sfaccioli <selefaccioli@gmail.com>
2022-08-18 07:27:24 +0200

    Initial commit

---
+++ insided-challenge/src/Components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Insided challenge</h1>
+    </div>
+  );
+}
+
+export default App;
